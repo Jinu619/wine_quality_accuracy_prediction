577dabaf53b2936e1a79da133a30f5328517fef6
Jinu619 <79348059+Jinu619@users.noreply.github.com>
2021-06-10 16:45:06 +0530

    Create wine_quality_accuracy_prediction.py

---
+++ wine_quality_accuracy_prediction.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import seaborn as sns
+import numpy as np
+
+wine = pd.read_csv('F://winequality-red1.csv')#import csv file
+wine.head()
+wine.shape
+
+print(wine.isna().sum())#finding null values
+
+#plot
+corr = wine.corr()
+sns.heatmap(corr,annot=True)
+
+# Create Classification version of target variable
+wine['goodquality'] = [1 if x >= 7 else 0 for x in wine['quality']]# Separate feature variables and target variable
+X = wine.drop(['quality','goodquality'], axis = 1)
+Y = wine['goodquality']
+
+# See proportion of good vs bad wines
+wine['goodquality'].value_counts()
+
+#splitting dataset
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.3,random_state=7)
+
+
+#using KNN
+from sklearn.neighbors import KNeighborsClassifier
+model = KNeighborsClassifier(n_neighbors=3)
+model.fit(X_train,Y_train)
+y_pred = model.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print("Accuracy Score:",accuracy_score(Y_test,y_pred))
+
+#using DT
+from sklearn.tree import DecisionTreeClassifier
+model = DecisionTreeClassifier(criterion='entropy',random_state=7)
+model.fit(X_train,Y_train)
+y_pred = model.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print("Accuracy Score:",accuracy_score(Y_test,y_pred))
+
+#using RF
+from sklearn.ensemble import RandomForestClassifier
+model2 = RandomForestClassifier(random_state=1)
+model2.fit(X_train, Y_train)
+y_pred2 = model2.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print("Accuracy Score:",accuracy_score(Y_test,y_pred2))
